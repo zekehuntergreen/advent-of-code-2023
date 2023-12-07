647bea57ade2578c09c037cf3309521f583c105e
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2023-12-07 20:09:18 +0000

    day 7 cleanup

--- day7/day7.py
+++ day7/day7.py
@@ -2,24 +2,8 @@ from functools import total_ordering
 
 PART_2 = True
 
-
-@total_ordering
-class Card:
-    face_card_values = {"A": 14, "K": 13, "Q": 12, "J": 1 if PART_2 else 11, "T": 10}
-
-    def __init__(self, value):
-        self.value = value
-
-    def __eq__(self, other):
-        return self.value == other.value
-
-    def __lt__(self, other):
-        return self.__get_card_score(self.value) < self.__get_card_score(other.value)
-
-    def __get_card_score(self, value):
-        if value in self.face_card_values:
-            value = self.face_card_values[value]
-        return int(value)
+card_values = {str(n): n for n in range(1, 10)}
+card_values.update({"A": 14, "K": 13, "Q": 12, "J": 1 if PART_2 else 11, "T": 10})
 
 
 @total_ordering
@@ -35,7 +19,7 @@ class Hand:
     ]
 
     def __init__(self, hand, wager):
-        self.hand = [Card(c) for c in hand]
+        self.hand = hand
         self.wager = wager
         self.type_score = (
             self.__get_type_score_part_2(hand)
@@ -48,7 +32,7 @@ class Hand:
             return self.type_score < other.type_score
         for i, this_card in enumerate(self.hand):
             if this_card != other.hand[i]:
-                return this_card < other.hand[i]
+                return card_values[this_card] < card_values[other.hand[i]]
 
     def __get_type_score(self, hand):
         hand_dict = {}
@@ -62,15 +46,12 @@ class Hand:
     def __get_type_score_part_2(self, hand):
         num_jokers = len([c for c in hand if c == "J"])
         hand_without_jokers = hand.replace("J", "")
-        hand_values = self.__get_type_score(hand_without_jokers)
-        if not hand_values:  # handles "JJJJJ"
-            hand_values = [0]
+        hand_values = self.__get_type_score(hand_without_jokers) or [0]
         hand_values[0] += num_jokers
         return self.hand_type_values.index(hand_values)
 
     def __get_type_score_part_1(self, hand):
-        hand_values = self.__get_type_score(hand)
-        return self.hand_type_values.index(hand_values)
+        return self.hand_type_values.index(self.__get_type_score(hand))
 
 
 hands = []
