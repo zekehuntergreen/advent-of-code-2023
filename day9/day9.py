55170e950488e23113105215a3aefa4f2a3af5fd
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2023-12-09 12:19:41 +0000

    day 9 cleanup
    single recursive call

--- day9/day9.py
+++ day9/day9.py
@@ -1,13 +1,13 @@
-def find_next(l, prev=False):
+def find_next(l):
     if set(l) == {0}:
-        return 0
-    diff = [l[i + 1] - l[i] for i in range(len(l) - 1)]
-    return l[0] - find_next(diff, prev) if prev else l[-1] + find_next(diff)
+        return 0, 0
+    prev, next = find_next([l[i + 1] - l[i] for i in range(len(l) - 1)])
+    return l[0] - prev, l[-1] + next
 
 
 part1, part2 = 0, 0
 for line in open("input/puzzle_input.txt").readlines():
-    line = list(map(lambda x: int(x), line.split()))
-    part1 += find_next(line)
-    part2 += find_next(line, True)
+    prev, next = find_next(list(map(lambda x: int(x), line.split())))
+    part1 += next
+    part2 += prev
 print(f"part 1: {part1} \t part 2: {part2}")
